78085824e7b4816fe2ab18d1e82a47d28efe8432
Jeffrey Shaw <jeffrey.s.shaw4@gmail.com>
2014-03-07 16:11:12 -0800

    07/Mar/2014
    Copied files from OneDrive

---
+++ Solution Files(Classes)/Connection.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfApplication5
+{
+    public class Connection
+    {
+    }
+
+    public class SQL_Connection
+    {
+        
+    }
+
+    public class SMTP_EmailConnect
+    {
+    }
+}
---
+++ UseCases.mm
@@ -0,0 +1,115 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1283093380553" ID="ID_1723255651" MODIFIED="1390761445520" TEXT="Project">
+<hook NAME="AutomaticEdgeColor"/>
+<hook NAME="MapStyle">
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode COLOR="#000000" LOCALIZED_TEXT="default" MAX_WIDTH="600" STYLE="as_parent">
+<font/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge/>
+<cloud/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode COLOR="#18898b" LOCALIZED_TEXT="styles.topic" STYLE="fork">
+<font/>
+</stylenode>
+<stylenode COLOR="#cc3300" LOCALIZED_TEXT="styles.subtopic" STYLE="fork">
+<font/>
+</stylenode>
+<stylenode COLOR="#669900" LOCALIZED_TEXT="styles.subsubtopic">
+<font/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode COLOR="#000000" LOCALIZED_TEXT="AutomaticLayout.level.root">
+<font/>
+</stylenode>
+<stylenode COLOR="#0033ff" LOCALIZED_TEXT="AutomaticLayout.level,1">
+<font/>
+</stylenode>
+<stylenode COLOR="#00b439" LOCALIZED_TEXT="AutomaticLayout.level,2">
+<font/>
+</stylenode>
+<stylenode COLOR="#990000" LOCALIZED_TEXT="AutomaticLayout.level,3">
+<font/>
+</stylenode>
+<stylenode COLOR="#111111" LOCALIZED_TEXT="AutomaticLayout.level,4">
+<font/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node CREATED="1390761447301" ID="ID_336114354" MODIFIED="1390761451706" POSITION="right" TEXT="Customer">
+<edge COLOR="#ff0000"/>
+</node>
+<node CREATED="1390761452244" ID="ID_1936126161" MODIFIED="1390761463670" POSITION="right" TEXT="League">
+<edge COLOR="#0000ff"/>
+</node>
+<node CREATED="1390761464583" ID="ID_366169299" MODIFIED="1390761470411" POSITION="right" TEXT="Comittment">
+<edge COLOR="#00ff00"/>
+</node>
+<node CREATED="1390761471309" ID="ID_1807011605" MODIFIED="1390761490027" POSITION="right" TEXT="Signed Up Verification (checked)">
+<edge COLOR="#ff00ff"/>
+</node>
+<node CREATED="1390761712024" ID="ID_618673362" MODIFIED="1391795039958" POSITION="left" TEXT="Startup">
+<node CREATED="1391794348819" ID="ID_198442453" MODIFIED="1391794362088" TEXT="Sign-Up">
+<node CREATED="1390761491912" ID="ID_289027267" MODIFIED="1391795070485" TEXT="Contact Info">
+<edge COLOR="#00007c"/>
+<node CREATED="1390761559175" ID="ID_25597604" MODIFIED="1390761578353" TEXT="Name"/>
+<node CREATED="1390761579312" ID="ID_1666527979" MODIFIED="1390761586574" TEXT="Phone Number"/>
+<node CREATED="1390761587259" ID="ID_1139272724" MODIFIED="1390761589412" TEXT="Email"/>
+<node CREATED="1390761589856" ID="ID_972017687" MODIFIED="1390761594795" TEXT="Insured"/>
+<node CREATED="1390761595286" ID="ID_1338275181" MODIFIED="1390761604565" TEXT="Emergency Contact"/>
+</node>
+<node CREATED="1390761755524" ID="ID_1283594668" MODIFIED="1391794377165" TEXT="Validate Email"/>
+<node CREATED="1390761745046" ID="ID_1301759228" MODIFIED="1391794379837" TEXT="Enter Email"/>
+</node>
+<node CREATED="1391794362891" ID="ID_306210758" MODIFIED="1391794368995" TEXT="Sign-In"/>
+</node>
+<node CREATED="1390761519600" ID="ID_561800985" MODIFIED="1390761554222" POSITION="left" TEXT="List">
+<edge COLOR="#007c00"/>
+<node CREATED="1390761610354" ID="ID_1834641065" MODIFIED="1390761624815" TEXT="Team List">
+<node CREATED="1390761626791" ID="ID_1191293855" MODIFIED="1390761636189" TEXT="Roster"/>
+</node>
+</node>
+<node CREATED="1390761649623" ID="ID_29315547" MODIFIED="1390761658327" POSITION="left" TEXT="Captain">
+<edge COLOR="#7c7c00"/>
+<node CREATED="1391796063423" ID="ID_1316950159" MODIFIED="1391796076892" TEXT="Roster Management">
+<node CREATED="1390761694000" ID="ID_1413142674" MODIFIED="1391796102432" TEXT="Post Team Roster"/>
+<node CREATED="1390761673363" ID="ID_1939848168" MODIFIED="1391796108505" TEXT="List Members info"/>
+<node CREATED="1390761659427" ID="ID_1282316763" MODIFIED="1391796114616" TEXT="List Current Members"/>
+</node>
+<node CREATED="1391796040183" ID="ID_1826072169" MODIFIED="1391796104976" TEXT="Form Management">
+<node CREATED="1391796118502" ID="ID_1904644246" MODIFIED="1391796156219" TEXT="View Forms"/>
+<node CREATED="1391796156798" ID="ID_1472081164" MODIFIED="1391796172966" TEXT="Validate Forms"/>
+</node>
+<node CREATED="1391796078367" ID="ID_910171653" MODIFIED="1391796086811" TEXT="Notification">
+<node CREATED="1390761680687" ID="ID_876198846" MODIFIED="1391796093769" TEXT="Send Email Reminders"/>
+<node CREATED="1391796199086" ID="ID_1241629657" MODIFIED="1391796230627" TEXT="View notifications"/>
+</node>
+</node>
+<node CREATED="1390761705145" ID="ID_821232664" MODIFIED="1391796283064" POSITION="left" TEXT="Player">
+<edge COLOR="#0000ff"/>
+<node CREATED="1390761726380" ID="ID_1170603137" MODIFIED="1390761732184" TEXT="Follow Up">
+<node CREATED="1391793567959" ID="ID_1651675648" MODIFIED="1391793582309" TEXT="View Status"/>
+<node CREATED="1391793583344" ID="ID_1799049543" MODIFIED="1391793592113" TEXT="File form"/>
+</node>
+<node CREATED="1390761732628" ID="ID_1555728052" MODIFIED="1390761742326" TEXT="Phone Call from Team Lead"/>
+</node>
+<node CREATED="1391796254965" ID="ID_1139801620" MODIFIED="1391796281129" POSITION="left" TEXT="End Program">
+<node CREATED="1391796286805" ID="ID_70361442" MODIFIED="1391796325618" TEXT="Confirmation"/>
+<node CREATED="1391796326221" ID="ID_1700932958" MODIFIED="1391796330973" TEXT="Exit"/>
+</node>
+</node>
+</map>
