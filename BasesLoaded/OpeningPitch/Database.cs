9e609fe9e4f1389dd232015813a47ef9f8e70201
Jeffrey Shaw <jeffrey.s.shaw4@gmail.com>
2014-03-11 13:49:00 -0700

    Update Project_11March2014
    Updated

---
+++ BasesLoaded/OpeningPitch/Database.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfApplication5
+{
+    public class Database
+    {
+        public void Create_Username()
+        {
+        }
+        public void Create_Password()
+        {
+        }
+        public void Login()
+        {
+        }
+        public void Forgot_Password()
+        {
+        }
+        public void View()
+        {
+        }
+    }
+    public class TC_DB : Database
+    {
+        public void Delete()
+        {
+        }
+        public void Approve()
+        {
+        }
+        public void Deny()
+        {
+        }
+        public void DisplayForms()
+        {
+        }
+        public void Notify()
+        {
+        }
+        public void Edit()
+        {
+        }
+    }
+    public class Player_DB : Database
+    {
+        public void Add_info()
+        {
+        }
+        public void View()
+        {
+        }
+        public void Edit_info()
+        {
+        }
+    }
+}
---
+++ BasesLoaded/OpeningPitch/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BasesLoaded    
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Log_In(object sender, RoutedEventArgs e)
+        {
+            //if ( = null)
+            //{
+            MessageBox.Show("You did not enter a valid Username and/or Password");
+            //}
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("Are you sure you would like to Exit the program?");
+
+            //MessageBoxResult result = MessageBox.Show("Are you sure you would like to exit the program?", "Exit", MessageBoxButton.OK);
+
+            //if (result == MessageBoxResult.OK)
+            //{
+            //    this.Close();
+            //      Application curApp = Application.Current;
+            //    curApp.Shutdown();
+            //}
+
+            MessageBoxResult result = MessageBox.Show("Do you want to close this window?",
+                "Confirmation", MessageBoxButton.OKCancel);
+
+            if (result == MessageBoxResult.OK)
+            {
+                Application curApp = Application.Current;
+                curApp.Shutdown();
+            }
+            else if (result == MessageBoxResult.Cancel)
+            {
+                
+            }
+        }
+
+        
+    }
+}
