8015f8cf4425a066853f2ef47c5d0ba21a424d39
Jeffrey Shaw <jeffrey.s.shaw4@gmail.com>
2014-03-15 21:31:45 -0700

    Trying to validate email on entry

--- BasesLoaded/OpeningPitch/RegisterWindow.xaml.cs
+++ BasesLoaded/OpeningPitch/RegisterWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,9 +15,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using System.Net;
-using System.Net.Mail;
-using System.Net.Mime;
+
+
 
 namespace OpeningPitch
 {
@@ -26,6 +29,7 @@ namespace OpeningPitch
         public RegisterWindow()
         {
             InitializeComponent();
+            First_Name_Input.Focus();
         }
 
         private void Register_Button_Click(object sender, RoutedEventArgs e)
@@ -36,29 +40,29 @@ namespace OpeningPitch
             //}
             //else
             //{
-                try
-                {
-                    
-                    SmtpClient client = new SmtpClient("smtp.live.com", 587);
-                    client.EnableSsl = true;
-                    client.Timeout = 10000;
-                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
-                    client.UseDefaultCredentials = false;
-                    client.Credentials = new NetworkCredential("basesloadedapp@outlook.com", "!QAZ@WSX1qaz2wsx");
+            try
+            {
 
-                    MailMessage msg = new MailMessage();
-                    msg.To.Add(E_Mail_Input.Text);
-                    msg.From = new MailAddress("basesloadedapp@outlook.com");
-                    msg.Subject = "Registration Successful";
-                    msg.Body = "Congratulations!\nPlease follow the link below to verify your submission.";
-                    client.Send(msg);
-                    MessageBox.Show("Please check your E-Mail for a verification link.");
-                }
-                catch (Exception ex)
-                {
+                SmtpClient client = new SmtpClient("smtp.live.com", 587);
+                client.EnableSsl = true;
+                client.Timeout = 10000;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential("basesloadedapp@outlook.com", "!QAZ@WSX1qaz2wsx");
 
-                    MessageBox.Show(ex.ToString());
-                }
+                MailMessage msg = new MailMessage();
+                msg.To.Add(Email_Input.Text);
+                msg.From = new MailAddress("basesloadedapp@outlook.com");
+                msg.Subject = "Registration Successful";
+                msg.Body = "Congratulations!\nPlease follow the link below to verify your submission.";
+                client.Send(msg);
+                MessageBox.Show( "Please check your E-Mail for a verification link.");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
             //}
         }
 
@@ -82,5 +86,42 @@ namespace OpeningPitch
         {
 
         }
+
+        private void Cancel_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you would like to cancel your Registration?\n\nAll data will be lost.",
+               "Confirmation", MessageBoxButton.OKCancel);
+
+            if (result == MessageBoxResult.OK)
+            {
+                MainWindow BacktoMain = new MainWindow();
+                BacktoMain.Show();
+            }
+            else if (result == MessageBoxResult.Cancel)
+            {
+
+            }
+        }
+
+               
     }
 }
+
+            //if (Email_Input.Text.Length <= 0)
+            //{
+            //    MessageBox.Show("Enter an email.");
+            //    Email_Input.Focus();
+            //}
+                 
+            //else if (!Regex.IsMatch(Email_Input.Text, @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|"
+            //+ @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
+            //+ @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$"))
+            //{
+            //    MessageBox.Show("Enter a Valid Email.");
+            //    Email_Input.Clear();
+            //    Email_Input.Focus();
+            //}
+    
+
+ 
+
